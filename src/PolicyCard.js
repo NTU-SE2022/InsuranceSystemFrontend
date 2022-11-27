8ae861c5c94828e65eda41e7341ea1593887d5f1
Jesse <ghjk85692012@gmail.com>
2022-11-27 23:12:42 +0800

    feat:add make claim function page

--- src/PolicyCard.js
+++ src/PolicyCard.js
@@ -34,6 +34,9 @@ export function BorderBox(props){
 }
 
 function BorderBoxContent(props){
+    if (props.value === undefined){
+        return
+    };
     return(
         <Box flex={1} sx={{height:'100%',textAlign:'left' ,overflow:"auto"}}>
         <Box component="p">Company Name:{props.value.companyname}</Box>
@@ -41,6 +44,72 @@ function BorderBoxContent(props){
         <Box component="p">Category:{props.value.category}</Box>
         <Box component='p'>Feature:{props.value.feature.map((policy=>(<Box component='li'>{policy}</Box>)))}</Box>
         <Box component='p'>Coverage:{props.value.coverage.map((policy=>(<Box component='li'>{policy}</Box>)))}</Box>
+        </Box>
+    )
+}
+
+// interface Claim {
+//     companyName: string;
+//     policeId: string;
+//     policyName: string;
+//     policyStatus: string;
+//     amount:number;
+//     effectiveDate: string;
+//     paymentMethod: string;
+//     paymentRecord:string;
+//     dueDate:string;
+//   }
+
+export function ClaimBox(props){
+    if (!props.value){
+        return(
+            <Box></Box>
+        )
+    };
+    return(
+        <Box
+        sx={{
+        display:'flex',
+        flexDirection: 'row',
+        boxShadow: 3,
+        display:'flex',
+        width: '50rem',
+        height: '30rem',
+        bgcolor: (theme) => (theme.palette.mode === 'dark' ? '#101010' : '#fff'),
+        color: (theme) =>
+            theme.palette.mode === 'dark' ? 'grey.300' : 'grey.800',
+        p: 1,
+        m: 1,
+        borderRadius: 2,
+        textAlign: 'center',
+        fontSize: '0.875rem',
+        fontWeight: '700',
+        }}
+        >
+        <Box sx={{flexGrow:10}}>
+            <ClaimBoxContent value={props.value}></ClaimBoxContent>
+        </Box>
+        <Box sx={{display:'flex',flexDirection:'column'}}>
+            <Box sx={{textAlign:'right',flexGrow:1}}><Button variant='outlined' fullWidth onClick={()=>props.onclick()}>Claim</Button></Box>
+        </Box>
+        </Box>
+    )
+}
+
+function ClaimBoxContent(props){
+    if (props.value === undefined){
+        return
+    };
+    return(
+        <Box flex={1} sx={{height:'100%',textAlign:'left' ,overflow:"auto"}}>
+        <Box component="p">Company Name:{props.value.companyName}</Box>
+        <Box component="p">Policy ID:{props.value.policyId}</Box>
+        <Box component="p">Policy Status:{props.value.policyStatus}</Box>
+        <Box component='p'>計畫數:{props.value.amount.toString()}</Box>
+        <Box component='p'>Policy Effective Date:{props.value.effectiveDate.toString()}</Box>
+        <Box component="p">Payment Method:{props.value.paymentMethod}</Box>
+        <Box component="p">Payment Record:{props.value.paymentRecord}</Box>
+        <Box component="p">Due Date:{props.value.dueDate.toString()}</Box>
         </Box>
     )
 }
